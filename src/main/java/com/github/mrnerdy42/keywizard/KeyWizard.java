941b29cd581a3d66e9a2af782b14a8247e87829c
MrNerdy42 <jdredmond2000@gmail.com>
2017-07-09 09:50:05 -0400

    Marked mod as client side only

--- src/main/java/com/github/mrnerdy42/keywizard/KeyWizard.java
+++ src/main/java/com/github/mrnerdy42/keywizard/KeyWizard.java
@@ -19,12 +19,12 @@ import com.github.mrnerdy42.keywizard.handlers.GuiHandler;
 
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = KeyWizard.MODID, name = KeyWizard.MODNAME, version = KeyWizard.VERSION, useMetadata = true)
+@Mod(modid = KeyWizard.MODID, name = KeyWizard.MODNAME, version = KeyWizard.VERSION, useMetadata = true, clientSideOnly = true)
 public class KeyWizard {
 	
 	public static final String MODID = "keywizard";
 	public static final String MODNAME = "Keyboard Wizard";
-	public static final String VERSION = "1.10.2-1.4.4";
+	public static final String VERSION = "1.10.2-1.4.5";
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
