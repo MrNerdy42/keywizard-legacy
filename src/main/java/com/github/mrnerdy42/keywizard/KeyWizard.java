6ce31c91d8bb86e4df7ef26e931351d86a5270bf
MrNerdy42 <jdredmond2000@gmail.com>
2017-08-01 15:52:57 -0400

    Updated version number

--- src/main/java/com/github/mrnerdy42/keywizard/KeyWizard.java
+++ src/main/java/com/github/mrnerdy42/keywizard/KeyWizard.java
@@ -24,7 +24,7 @@ public class KeyWizard {
 	
 	public static final String MODID = "keywizard";
 	public static final String MODNAME = "Keyboard Wizard";
-	public static final String VERSION = "1.10.2-1.4.5";
+	public static final String VERSION = "1.10.2-1.5";
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
