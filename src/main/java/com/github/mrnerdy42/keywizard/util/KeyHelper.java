c1a6303cd74cd55d588ee1584185bb9c2f5ca782
MrNerdy42 <jdredmond2000@gmail.com>
2018-12-18 21:14:03 -0500

    Removed ugly switch statement

--- src/main/java/com/github/mrnerdy42/keywizard/util/KeyHelper.java
+++ src/main/java/com/github/mrnerdy42/keywizard/util/KeyHelper.java
@@ -31,77 +31,48 @@ import java.util.HashMap;
 
 public class KeyHelper {
 	
-	private static final HashMap<Integer, String> FUNCTION_KEYS = new HashMap();
+	private static final HashMap<Integer, String> KEY_LABELS = new HashMap();
 	static{
-		FUNCTION_KEYS.put(KEY_F1, "F1");
-		FUNCTION_KEYS.put(KEY_F2, "F2");
-		FUNCTION_KEYS.put(KEY_F3, "F3");
-		FUNCTION_KEYS.put(KEY_F4, "F4");
-		FUNCTION_KEYS.put(KEY_F5, "F5");
-		FUNCTION_KEYS.put(KEY_F6, "F6");
-		FUNCTION_KEYS.put(KEY_F7, "F7");
-		FUNCTION_KEYS.put(KEY_F8, "F8");
-		FUNCTION_KEYS.put(KEY_F9, "F9");
-		FUNCTION_KEYS.put(KEY_F10, "F10");
-		FUNCTION_KEYS.put(KEY_F11, "F11");
-		FUNCTION_KEYS.put(KEY_F12, "F12");
+		KEY_LABELS.put(KEY_F1, "F1");
+		KEY_LABELS.put(KEY_F2, "F2");
+		KEY_LABELS.put(KEY_F3, "F3");
+		KEY_LABELS.put(KEY_F4, "F4");
+		KEY_LABELS.put(KEY_F5, "F5");
+		KEY_LABELS.put(KEY_F6, "F6");
+		KEY_LABELS.put(KEY_F7, "F7");
+		KEY_LABELS.put(KEY_F8, "F8");
+		KEY_LABELS.put(KEY_F9, "F9");
+		KEY_LABELS.put(KEY_F10, "F10");
+		KEY_LABELS.put(KEY_F11, "F11");
+		KEY_LABELS.put(KEY_F12, "F12");
+		KEY_LABELS.put(KEY_MINUS, "-");
+		KEY_LABELS.put(KEY_EQUALS, "=");
+		KEY_LABELS.put(KEY_LBRACKET, "[");
+		KEY_LABELS.put(KEY_RBRACKET, "]");
+		KEY_LABELS.put(KEY_SEMICOLON, ";");
+		KEY_LABELS.put(KEY_COMMA, ",");
+		KEY_LABELS.put(KEY_PERIOD, ".");
+		KEY_LABELS.put(KEY_APOSTROPHE, "'");
+		KEY_LABELS.put(KEY_SLASH, "/");
+		KEY_LABELS.put(KEY_BACKSLASH, "\\");
+		KEY_LABELS.put(KEY_GRAVE, "`");
+		KEY_LABELS.put(KEY_LCONTROL, "LCTRL");
+		KEY_LABELS.put(KEY_RCONTROL, "RCTRL");
 	}
 	
 	/**
-	 * Translate the LWJGL key name to a symbol or shortened version of the name
+	 * Translate the LWJGL key name to a symbol or shortened version of the name.
+	 * Returns the long version of the key name if there is no symbol available.
 	 * 
 	 * @param id
 	 *            the LWJGL code of the key to translate
 	 */
 	public static String translateKey(int id) {
 		String keyName;
-		if (FUNCTION_KEYS.containsKey(id)) {
-			return FUNCTION_KEYS.get(id);
+		if (KEY_LABELS.containsKey(id)) {
+			return KEY_LABELS.get(id);
+		} else {
+			return getKeyName(id);
 		}
-		switch (id) {
-		case KEY_MINUS:
-			keyName = "-";
-			break;
-		case KEY_EQUALS:
-			keyName = "=";
-			break;
-		case KEY_LBRACKET:
-			keyName = "[";
-			break;
-		case KEY_RBRACKET:
-			keyName = "]";
-			break;
-		case KEY_SEMICOLON:
-			keyName = ";";
-			break;
-		case KEY_COMMA:
-			keyName = ",";
-			break;
-		case KEY_PERIOD:
-			keyName = ".";
-			break;
-		case KEY_APOSTROPHE:
-			keyName = "'";
-			break;
-		case KEY_SLASH:
-			keyName = "/";
-			break;
-		case KEY_BACKSLASH:
-			keyName = "\\";
-			break;
-		case KEY_GRAVE:
-			keyName = "`";
-			break;
-		case KEY_LCONTROL:
-			keyName = "LCTRL";
-			break;
-		case KEY_RCONTROL:
-			keyName = "RCTRL";
-			break;
-		default:
-			keyName = getKeyName(id);
-			break;
-		}
-		return keyName;
 	}
 }
