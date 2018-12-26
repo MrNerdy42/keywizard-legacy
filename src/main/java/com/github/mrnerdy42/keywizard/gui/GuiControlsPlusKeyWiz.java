f2aa08458b0ee792495c85c1639f47413b8fbc1f
MrNerdy42 <jdredmond2000@gmail.com>
2018-12-26 18:05:00 -0500

    Controls menu button Began adding a button for Keyboard Wizard to the controls menu

---
+++ src/main/java/com/github/mrnerdy42/keywizard/gui/GuiControlsPlusKeyWiz.java
@@ -0,0 +1,13 @@
+package com.github.mrnerdy42.keywizard.gui;
+
+import net.minecraft.client.gui.GuiControls;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.settings.GameSettings;
+
+public class GuiControlsPlusKeyWiz extends GuiControls{
+
+	public GuiControlsPlusKeyWiz(GuiScreen screen, GameSettings settings) {
+		super(screen, settings);
+	}
+	
+}
