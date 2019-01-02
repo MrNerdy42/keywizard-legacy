21e802d4fbdaf442dd1310b6828af30c825eb6ce
MrNerdy42 <jdredmond2000@gmail.com>
2019-01-02 18:55:11 -0500

    Added button Added a currently non-working button to the controls gui to open keyboard wizard

--- src/main/java/com/github/mrnerdy42/keywizard/gui/GuiControlsPlusKeyWiz.java
+++ src/main/java/com/github/mrnerdy42/keywizard/gui/GuiControlsPlusKeyWiz.java
@@ -3,46 +3,51 @@ package com.github.mrnerdy42.keywizard.gui;
 import java.io.IOException;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;//	@Override
-//	public void initGui() {
-//		super.initGui();
-//		
-//		this.keywizardButton = new GuiButton(0, this.height/2, this.width/2, 100, 20, I18n.format("options.OpenKeyWizard"));
-//		this.buttonList.add(this.keywizardButton);
-//		System.out.println("Hello World!");
-//	}
-
-import net.minecraft.client.gui.GuiControls;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiKeyBindingList;
 import net.minecraft.client.gui.GuiOptionButton;
 import net.minecraft.client.gui.GuiOptionSlider;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.client.resources.I18n;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class GuiControlsPlusKeyWiz extends GuiControls{
-	
-	
-	private static final GameSettings.Options[] OPTIONS_ARR = new GameSettings.Options[] {GameSettings.Options.INVERT_MOUSE, GameSettings.Options.SENSITIVITY, GameSettings.Options.TOUCHSCREEN, GameSettings.Options.AUTO_JUMP};
-	private final GameSettings options;
-	private final GuiScreen parentScreen;
-	
-	private GuiButton buttonReset;
-	private GuiButton buttonKeyWizard;
-	private GuiKeyBindingList keyBindingList;
-	
-	public GuiControlsPlusKeyWiz(GuiScreen screen, GameSettings settings) {
-		super(screen, settings);
-		this.options = settings;
-		this.parentScreen = screen;
-	}
-	
-	@Override
-    public void initGui() {
-        this.keyBindingList = new GuiKeyBindingList(this, this.mc);
+@SideOnly(Side.CLIENT)
+public class GuiControlsPlusKeyWiz extends GuiScreen
+{
+    private static final GameSettings.Options[] OPTIONS_ARR = new GameSettings.Options[] {GameSettings.Options.INVERT_MOUSE, GameSettings.Options.SENSITIVITY, GameSettings.Options.TOUCHSCREEN, GameSettings.Options.AUTO_JUMP};
+    /** A reference to the screen object that created this. Used for navigating between screens. */
+    private final GuiScreen parentScreen;
+    protected String screenTitle = "Controls";
+    /** Reference to the GameSettings object. */
+    private final GameSettings options;
+    /** The ID of the button that has been pressed. */
+    public KeyBinding buttonId;
+    public long time;
+    private GuiControlsPlusKeyWizBindingList keyBindingList;
+    private GuiButton buttonReset;
+    private GuiButton buttonOpenKeyWiz;
+
+    public GuiControlsPlusKeyWiz(Minecraft mcIn, GuiScreen screen, GameSettings settings)
+    {
+        this.parentScreen = screen;
+        this.options = settings;
+        this.mc = mcIn;
+    }
+
+    /**
+     * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
+     * window resizes, the buttonList is cleared beforehand.
+     */
+    public void initGui()
+    {
+        this.keyBindingList = new GuiControlsPlusKeyWizBindingList(this, this.mc);
         this.buttonList.add(new GuiButton(200, this.width / 2 - 155 + 160, this.height - 29, 150, 20, I18n.format("gui.done")));
-        this.buttonReset = this.addButton(new GuiButton(201, this.width / 2 - 155, this.height - 29, 150, 20, I18n.format("controls.resetAll")));
+        this.buttonReset = this.addButton(new GuiButton(201, this.width / 2 - 155, this.height - 29, 75, 20, I18n.format("controls.resetAll")));
+        this.buttonOpenKeyWiz = new GuiButton(203, this.width / 2 - 75, this.height - 29, 75, 20, I18n.format("gui.openKeyWiz"));
+        this.buttonList.add(this.buttonOpenKeyWiz);
         this.screenTitle = I18n.format("controls.title");
         int i = 0;
 
@@ -60,49 +65,46 @@ public class GuiControlsPlusKeyWiz extends GuiControls{
             ++i;
         }
     }
-	
-	@Override
-	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		this.drawDefaultBackground();
-		this.keyBindingList.drawScreen(mouseX, mouseY, partialTicks);
-		this.drawCenteredString(this.fontRenderer, this.screenTitle, this.width / 2, 8, 16777215);
-		boolean flag = false;
 
-		for (KeyBinding keybinding : this.options.keyBindings) {
-			if (!keybinding.isSetToDefaultValue()) {
-				flag = true;
-				break;
-			}
-		}
+    /**
+     * Handles mouse input.
+     */
+    public void handleMouseInput() throws IOException
+    {
+        super.handleMouseInput();
+        this.keyBindingList.handleMouseInput();
+    }
 
-		this.buttonReset.enabled = flag;
-		super.drawScreen(mouseX, mouseY, partialTicks);
-	}
+    /**
+     * Called by the controls from the buttonList when activated. (Mouse pressed for buttons)
+     */
+    protected void actionPerformed(GuiButton button) throws IOException
+    {
+        if (button.id == 200)
+        {
+            this.mc.displayGuiScreen(this.parentScreen);
+        }
+        else if (button.id == 201)
+        {
+            for (KeyBinding keybinding : this.mc.gameSettings.keyBindings)
+            {
+                keybinding.setToDefault();
+            }
 
-	@Override
-	public void handleMouseInput() throws IOException {
-		super.handleMouseInput();
-		this.keyBindingList.handleMouseInput();
-	}
+            KeyBinding.resetKeyBindingArrayAndHash();
+        }
+        else if (button.id < 100 && button instanceof GuiOptionButton)
+        {
+            this.options.setOptionValue(((GuiOptionButton)button).returnEnumOptions(), 1);
+            button.displayString = this.options.getKeyBinding(GameSettings.Options.getEnumOptions(button.id));
+        }
+    }
 
-	@Override
-	protected void actionPerformed(GuiButton button) throws IOException {
-		if (button.id == 200) {
-			this.mc.displayGuiScreen(this.parentScreen);
-		} else if (button.id == 201) {
-			for (KeyBinding keybinding : this.mc.gameSettings.keyBindings) {
-				keybinding.setToDefault();
-			}
-
-			KeyBinding.resetKeyBindingArrayAndHash();
-		} else if (button.id < 100 && button instanceof GuiOptionButton) {
-			this.options.setOptionValue(((GuiOptionButton) button).returnEnumOptions(), 1);
-			button.displayString = this.options.getKeyBinding(GameSettings.Options.getEnumOptions(button.id));
-		}
-	}
-	
-	@Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+    /**
+     * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
+     */
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
+    {
         if (this.buttonId != null)
         {
             this.buttonId.setKeyModifierAndCode(net.minecraftforge.client.settings.KeyModifier.getActiveModifier(), -100 + mouseButton);
@@ -115,4 +117,73 @@ public class GuiControlsPlusKeyWiz extends GuiControls{
             super.mouseClicked(mouseX, mouseY, mouseButton);
         }
     }
-}	
+
+    /**
+     * Called when a mouse button is released.
+     */
+    protected void mouseReleased(int mouseX, int mouseY, int state)
+    {
+        if (state != 0 || !this.keyBindingList.mouseReleased(mouseX, mouseY, state))
+        {
+            super.mouseReleased(mouseX, mouseY, state);
+        }
+    }
+
+    /**
+     * Fired when a key is typed (except F11 which toggles full screen). This is the equivalent of
+     * KeyListener.keyTyped(KeyEvent e). Args : character (character on the key), keyCode (lwjgl Keyboard key code)
+     */
+    protected void keyTyped(char typedChar, int keyCode) throws IOException
+    {
+        if (this.buttonId != null)
+        {
+            if (keyCode == 1)
+            {
+                this.buttonId.setKeyModifierAndCode(net.minecraftforge.client.settings.KeyModifier.NONE, 0);
+                this.options.setOptionKeyBinding(this.buttonId, 0);
+            }
+            else if (keyCode != 0)
+            {
+                this.buttonId.setKeyModifierAndCode(net.minecraftforge.client.settings.KeyModifier.getActiveModifier(), keyCode);
+                this.options.setOptionKeyBinding(this.buttonId, keyCode);
+            }
+            else if (typedChar > 0)
+            {
+                this.buttonId.setKeyModifierAndCode(net.minecraftforge.client.settings.KeyModifier.getActiveModifier(), typedChar + 256);
+                this.options.setOptionKeyBinding(this.buttonId, typedChar + 256);
+            }
+
+            if (!net.minecraftforge.client.settings.KeyModifier.isKeyCodeModifier(keyCode))
+            this.buttonId = null;
+            this.time = Minecraft.getSystemTime();
+            KeyBinding.resetKeyBindingArrayAndHash();
+        }
+        else
+        {
+            super.keyTyped(typedChar, keyCode);
+        }
+    }
+
+    /**
+     * Draws the screen and all the components in it.
+     */
+    public void drawScreen(int mouseX, int mouseY, float partialTicks)
+    {
+        this.drawDefaultBackground();
+        this.keyBindingList.drawScreen(mouseX, mouseY, partialTicks);
+        this.drawCenteredString(this.fontRenderer, this.screenTitle, this.width / 2, 8, 16777215);
+        boolean flag = false;
+
+        for (KeyBinding keybinding : this.options.keyBindings)
+        {
+            if (!keybinding.isSetToDefaultValue())
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        this.buttonReset.enabled = flag;
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+}
--- src/main/java/com/github/mrnerdy42/keywizard/handlers/ClientEventHandler.java
+++ src/main/java/com/github/mrnerdy42/keywizard/handlers/ClientEventHandler.java
@@ -26,8 +26,7 @@ public class ClientEventHandler {
     @SubscribeEvent
     public void guiInit(GuiOpenEvent e) {
         if (e.getGui() instanceof GuiControls && !(e.getGui() instanceof GuiControlsPlusKeyWiz)) {
-            e.setGui(new GuiControlsPlusKeyWiz(e.getGui(), this.client.gameSettings));
-            System.out.println("Overriding");
+            e.setGui(new GuiControlsPlusKeyWiz(this.client, e.getGui(), this.client.gameSettings));
         }
     }
 }
