027f9cb0dfbd1b382f9bdf4e300dd627f312f28b
MrNerdy42 <jdredmond2000@gmail.com>
2019-04-30 11:42:09 -0400

    Reworked keyboard structure

--- src/main/java/com/github/mrnerdy42/keywizard/gui/GuiKeyboard.java
+++ src/main/java/com/github/mrnerdy42/keywizard/gui/GuiKeyboard.java
@@ -14,57 +14,19 @@ public class GuiKeyboard extends FloatGui{
 	public double scaleFactor;
 	public GuiKeyWizard parent;
 	
-	protected GuiKeyRow[] rows;
-	protected KeyboardLayout layout;
+	private double yOffset = 0;
 
-	public GuiKeyboard(GuiKeyWizard parent, KeyboardLayout layout, double x, double y, double scaleFactor) {
+	public GuiKeyboard(GuiKeyWizard parent, double x, double y, double scaleFactor) {
 		this.parent = parent;
-		this.layout = layout;
 		this.x = x;
 		this.y = y;
 		this.scaleFactor = scaleFactor;
-		this.rows = new GuiKeyRow[layout.numRows()];
 	}
 	
-	public void init() {
-		for(int r=0;r<this.layout.numRows(); r++) {
-			this.rows[r] = new GuiKeyRow(this, r*);
-			for(int k=0;k<this.layout.rowLength(r);k++) {
-				this.
-			}
-		}
-	}
 	
 	public void draw(Minecraft mc, int mouseX, int mouseY) {
 	}
 	
 	public void addKey(GuiKeyboardKey k) {
 	}
-	
-	private class GuiKeyRow{
-		private ArrayList<GuiKeyboardKey> keys = new ArrayList<GuiKeyboardKey>();
-		
-		private GuiKeyboard keyboard;
-		
-		private int width=0;
-		private int heigh=0;
-		
-		private int xOffset = 0;
-		private int yOffset;
-		
-		private int xSpacing;
-		
-		private GuiKeyRow(GuiKeyboard keyboard, int yOffset, int xSpacing) {
-			this.keyboard = keyboard;
-			this.yOffset = yOffset;
-			this.xSpacing=xSpacing;
-	
-		}
-		
-		private void addKey(int[] keyIn) {
-			this.keys.add(new GuiKeyboardKey(this.keyboard, this.keyboard.x + this.xOffset, this.keyboard.y + this.yOffset, keyIn[0], keyIn[1], keyIn[3]));
-		}
-		
-	}
-
 }
--- src/main/java/com/github/mrnerdy42/keywizard/util/KeyboardFactory.java
+++ src/main/java/com/github/mrnerdy42/keywizard/util/KeyboardFactory.java
@@ -4,13 +4,20 @@ import com.github.mrnerdy42.keywizard.gui.GuiKeyWizard;
 import com.github.mrnerdy42.keywizard.gui.GuiKeyboard;
 
 public class KeyboardFactory {
-	public static GuiKeyboard makeKeyboard(KeyboardLayout layout, GuiKeyWizard parent, int x, int y) {
+	public static GuiKeyboard makeKeyboard(KeyboardLayout layout, GuiKeyWizard parent, double x, double y, double scaleFactor) {
 		switch(layout) {
 		case QWERTY:
-			break;
+			return makeQwertyKeyboard(parent, x, y, scaleFactor);
+		case NUMPAD:
+			return null;
+		default:
+			return null;
 		}
 	}
 	
-	private static makeQWERTYkeyboard
+	private static GuiKeyboard makeQwertyKeyboard(GuiKeyWizard parent, double x, double y, double scaleFactor) {
+		GuiKeyboard kb = new GuiKeyboard(parent, x, y, scaleFactor);
+		return kb;
+	}
 
 }
