d284f5bbf25dee0ec2059e8169792092690740df
MrNerdy42 <jdredmond2000@gmail.com>
2019-04-30 11:44:44 -0400

    Made GuiKeyboardKey an inner class of GuiKeyboard

--- src/main/java/com/github/mrnerdy42/keywizard/gui/GuiKeyboard.java
+++ src/main/java/com/github/mrnerdy42/keywizard/gui/GuiKeyboard.java
@@ -3,6 +3,7 @@ package com.github.mrnerdy42.keywizard.gui;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.github.mrnerdy42.keywizard.util.KeyHelper;
 import com.github.mrnerdy42.keywizard.util.KeyboardLayout;
 
 import net.minecraft.client.Minecraft;
@@ -29,4 +30,43 @@ public class GuiKeyboard extends FloatGui{
 	
 	public void addKey(GuiKeyboardKey k) {
 	}
+	
+	private class GuiKeyboardKey extends FloatGui{
+		public GuiKeyboard keyboard;
+		public double x;
+		public double y;
+		public double width;
+		public double height;
+		
+		public int keyCode;
+		public String displayString;
+		
+		protected boolean hovered;
+		
+		public GuiKeyboardKey(GuiKeyboard keyboard, double x, double y, double width, double height, int keyCode) {
+			this.keyboard = keyboard;
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+			this.keyCode = keyCode;
+			this.displayString = KeyHelper.translateKey(this.keyCode);
+		}
+		
+		public void drawKey(Minecraft mc, double mouseX, double mouseY ) {
+			this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
+			if(this.hovered) {
+				drawNoFillRect(this.x, this.y, this.x + this.width, this.y + this.height, 0xFF00FF00);
+			}else {
+				drawNoFillRect(this.x, this.y, this.x + this.width, this.y + this.height, 0xFFFFFFFF);
+			}
+			this.drawCenteredString(this.keyboard.parent.getFontRenderer(), this.displayString, (float)(this.x+(this.width+2)/2.0F), (float)(this.y+(this.height-7)/2.0F), 0xFFFFFF);
+		}
+		
+		protected void drawNoFillRect(double left, double top, double right, double bottom, int color) {
+			drawHorizontalLine(left, right, top, color);
+			drawHorizontalLine(left, right, bottom, color);
+			drawVerticalLine(left, top, bottom, color);
+			drawVerticalLine(right, top, bottom, color);
+		}
 }
