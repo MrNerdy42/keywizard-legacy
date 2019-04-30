93d16648af82bf5c626e95fc9acf9f844cab7de4
MrNerdy42 <jdredmond2000@gmail.com>
2019-04-30 10:42:07 -0400

    Switched to factory pattern for keyboards

---
+++ src/main/java/com/github/mrnerdy42/keywizard/util/KeyboardFactory.java
@@ -0,0 +1,16 @@
+package com.github.mrnerdy42.keywizard.util;
+
+import com.github.mrnerdy42.keywizard.gui.GuiKeyWizard;
+import com.github.mrnerdy42.keywizard.gui.GuiKeyboard;
+
+public class KeyboardFactory {
+	public static GuiKeyboard makeKeyboard(KeyboardLayout layout, GuiKeyWizard parent, int x, int y) {
+		switch(layout) {
+		case QWERTY:
+			break;
+		}
+	}
+	
+	private static makeQWERTYkeyboard
+
+}
