3a396c6a0b21446fc89d2e78aeb99501135b4745
MrNerdy42 <jdredmond2000@gmail.com>
2019-09-21 18:08:36 -0400

    Version push

--- src/main/java/com/github/mrnerdy42/keywizard/KeyWizard.java
+++ src/main/java/com/github/mrnerdy42/keywizard/KeyWizard.java
@@ -20,7 +20,7 @@ public class KeyWizard {
 	
 	public static final String MODID = "keywizard";
 	public static final String MODNAME = "Keyboard Wizard";
-	public static final String VERSION = "1.12.2-1.7";
+	public static final String VERSION = "1.12.2-1.7.1";
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
