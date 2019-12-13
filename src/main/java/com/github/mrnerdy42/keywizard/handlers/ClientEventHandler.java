ad79dc2593a30cc2465bf16f8a17c30a88ae2379
MrNerdy42 <jdredmond2000@gmail.com>
2019-12-12 20:02:02 -0500

    Rewrote controls gui override

--- src/main/java/com/github/mrnerdy42/keywizard/handlers/ClientEventHandler.java
+++ src/main/java/com/github/mrnerdy42/keywizard/handlers/ClientEventHandler.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiControls;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.event.GuiScreenEvent.ActionPerformedEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -30,30 +31,33 @@ public class ClientEventHandler {
 		ClientRegistry.registerKeyBinding(keyOpenKeyWizard);
 	}
 	
-    @SuppressWarnings("unchecked")
 	@SubscribeEvent
-    public void guiInit(InitGuiEvent e) {
-    	if (e.getGui() instanceof GuiControls && KeyWizardConfig.openFromControlsGui) {
+    public void controlsGuiInit(InitGuiEvent e) {
+    	if (KeyWizardConfig.openFromControlsGui && e.getGui() instanceof GuiControls ) {
     		int width = e.getGui().width;
     		int buttonY = 0;
     		for(GuiButton b: e.getButtonList()) {
-    			if(b.id == 200) {
+    			if(b.id == 200) { // Done
     				b.width = 100;
-    				buttonY = b.y;
     				b.x = width / 2 + 60;
+    				
+    				buttonY = b.y;
     			}
-    			if(b.id == 201) {
+    			if(b.id == 201) { // Reset All
     				b.width = 100;
+    				b.x = width / 2 - 160;
     			}
     		}
     		e.getButtonList().add(new GuiButton(203, width / 2 - 50, buttonY, 100, 20, I18n.format("gui.openKeyWiz")));
     	}
-        /*    	
-        if (e.getGui() instanceof GuiControls && !(e.getGui() instanceof GuiControlsPlusKeyWiz) && KeyWizardConfig.openFromControlsGui) {
-           e.setGui(new GuiControlsPlusKeyWiz(this.client, client.currentScreen, this.client.gameSettings));
-        }
-        */
     }
+	
+	@SubscribeEvent
+	public void controlsGuiActionPreformed(ActionPerformedEvent e) {
+		if (KeyWizardConfig.openFromControlsGui && e.getGui() instanceof GuiControls && e.getButton().id == 203) {
+			client.displayGuiScreen(new GuiKeyWizard(client, e.getGui()));
+		}
+	}
     
     @SubscribeEvent
     public void keyPressed(KeyInputEvent e) {
